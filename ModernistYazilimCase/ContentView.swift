e41570bc8236bc805a1f5479a6af01022ff53edd
Burak GÜL <developerburakgul@gmail.com>
2025-07-03 14:13:55 +0300

    Initial Commit

---
+++ ModernistYazilimCase/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ModernistYazilimCase
+//
+//  Created by Burak Gül on 3.07.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ModernistYazilimCase/ModernistYazilimCaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModernistYazilimCaseApp.swift
+//  ModernistYazilimCase
+//
+//  Created by Burak Gül on 3.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct ModernistYazilimCaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
